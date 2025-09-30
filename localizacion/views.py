3f569e25a2fe3705845e282a3ad954733b38b7f9
Adrian <adrian.silvasantiste@tecsup.edu.pe>
2025-09-30 03:56:14 -0500

    commit inicial

---
+++ localizacion/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def mapa_view(request):
+    # Valores por defecto (ejemplo: Lima)
+    lat = -12.0464
+    lng = -77.0428
+
+    if request.method == "POST":
+        try:
+            lat = float(request.POST.get("lat", lat))
+            lng = float(request.POST.get("lng", lng))
+        except ValueError:
+            pass  # Si meten basura, se queda con los valores por defecto
+
+    context = {
+        'lat': lat,
+        'lng': lng
+    }
+    return render(request, 'mapa.html', context)
